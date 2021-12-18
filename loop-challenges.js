c47762d3b6dc1d1e7a24810a797d0c21ad7139f1
beccamargolis <rmargolis78@gmail.com>
2021-12-17 16:30:40 -0800

    first commit

---
+++ loop-challenges.js
@@ -0,0 +1,35 @@
+//Using a loop write code that will console.log all of the odd values from 1 up to 20.
+for(var i=1; i<=20; i++) {
+    if(i % 2 == 1) {
+        console.log(i)
+    }
+}
+
+//Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+for(var i=100; i>=0; i--) {
+    if(i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+//Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+for(var i=4; i>=-3.5; i-=1.5) {
+    console.log(i)
+}
+
+
+//Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100.
+var sum = 0
+
+for(var i=1; i<=100; i++) {
+    sum +=i;
+    console.log(sum);
+}
+
+//Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12.
+var product = 1
+
+for(var i=1; i<=12; i++) {
+    product *=i;
+    console.log(product);
+}
